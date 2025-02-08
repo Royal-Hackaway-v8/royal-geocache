a9d59043592e08edab6de0ab57cbbbad65a62167
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-02-08 13:54:35 +0000

    Add: Overlay to hero
    Co-Authored-By: leobirch04 <161459890+leobirch04@users.noreply.github.com>
    Co-Authored-By: Jamuk504 <88954499+jamuk504@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@
 
 import { useEffect, useRef, useState } from "react";
 import PageView from "@/components/ui/PageView";
+import Link from "next/link";
 
 export default function Home() {
 	const videoRef = useRef<HTMLVideoElement>(null);
-	const [searchQuery, setSearchQuery] = useState("");
 	const [isPlaying, setIsPlaying] = useState(true);
 
 	useEffect(() => {
@@ -41,6 +41,7 @@ export default function Home() {
 
 	return (
 		<PageView>
+			{/* Background Video */}
 			<video
 				autoPlay
 				loop
@@ -49,31 +50,25 @@ export default function Home() {
 				preload="auto"
 				ref={videoRef}
 				className="fixed top-0 left-0 w-screen h-screen object-cover"
-				// poster="/images/video_placeholder.jpg"
 			>
 				<source src="/video/hero.mp4" type="video/mp4" />
 				Your browser doesn't support the video tag.
 			</video>
 
-			{/* Placeholder Image (when video paused) */}
-			{/* {!isPlaying && (
-				<img
-					src="/images/video_placeholder.jpg"
-					alt="Video Placeholder"
-					className="fixed top-0 left-0 w-screen h-screen object-cover"
-				/>
-			)} */}
-
-			{/* Hero Section */}
-			{/* <div className="relative w-full h-[75vh] flex justify-center items-center flex-col">
-				<img
-					src="/images/Oaxaca.svg"
-					className="h-[80vh] mix-blend-overlay"
-					style={{
-						filter: "drop-shadow(0 0 .5rem rgba(0, 0, 0, .5)) invert(1)",
-					}}
-				/>
-			</div> */}
+			{/* Overlay Content */}
+			<div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white text-center bg-black/40">
+				<h1 className="text-4xl md:text-6xl font-bold drop-shadow-lg text-green-400">
+					Adventure Awaits
+				</h1>
+				<h2 className="text-3xl italic md:text-6xl font-bold drop-shadow-lg text-green-400 opacity-80">
+					Start Your Hunt Today!
+				</h2>
+				<Link href="/manage">
+					<button className="mt-6 px-6 py-3 text-lg font-semibold bg-green-500 hover:bg-green-600 transition-all rounded-full shadow-lg">
+						Start Hunting
+					</button>
+				</Link>
+			</div>
 		</PageView>
 	);
 }
