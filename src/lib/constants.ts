b503beff4625e6711520bdeb705ce8273c93d03a
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-02-09 06:33:41 +0000

    Update constants.ts
    Co-Authored-By: Thomas <31802793+thomasnottom@users.noreply.github.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const CACHING_THRESHOLD = 0.5;
 export const WITHIN_RANGE_RADIUS = 10; // Distance to show green / stop showing red
 export const EARTH_RADIUS = 6371;
-export const TWO = 2;
+export const ONE = 1;
+export const TWO = ONE + ONE;
 
 export const COOLDOWN_PERIOD = 1000 * 60 * 60 * 12 * TWO; // a day
