446ce4bd2c1889686d879b04201354c7f38c7153
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-02-09 07:10:22 +0000

    Update Hero.tsx
    Co-Authored-By: Thomas <31802793+thomasnottom@users.noreply.github.com>

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Map from "./Map";
 
 const Hero = () => {
+	const [mounted, setMounted] = useState<boolean>(false);
+	useEffect(() => setMounted(true), []);
+
 	return (
 		<div className="w-screen h-screen  flex flex-col justify-center items-center">
 			<div className=" m-28 h-full w-full flex flex-col justify-center items-center ">
-				<Map />
+				{mounted && <Map />}
 			</div>
 		</div>
 	);
