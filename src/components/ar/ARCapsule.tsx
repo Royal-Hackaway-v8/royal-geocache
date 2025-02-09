897c3487a721e3e88f9c68c0c54aa112b2d041ef
Kenny <70860732+KennyOliver@users.noreply.github.com>
2025-02-09 06:37:24 +0000

    Update ARCapsule.tsx
    Co-Authored-By: Thomas <31802793+thomasnottom@users.noreply.github.com>

--- src/components/ar/ARCapsule.tsx
+++ src/components/ar/ARCapsule.tsx
@@ -1,33 +1,33 @@
-"use client";
-import React, { useEffect, useRef } from "react";
-import "@google/model-viewer";
+// "use client";
+// import React, { useEffect, useRef } from "react";
+// import "@google/model-viewer";
 
-const ARCapsule = () => {
-	const modelRef = useRef<HTMLDivElement>(null);
+// const ARCapsule = () => {
+// 	const modelRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		console.log("WebGLRenderingContext:", WebGLRenderingContext);
+// 	useEffect(() => {
+// 		console.log("WebGLRenderingContext:", WebGLRenderingContext);
 
-		if (modelRef.current) {
-			console.log("Model-Viewer Mounted!");
-		}
-	}, []);
+// 		if (modelRef.current) {
+// 			console.log("Model-Viewer Mounted!");
+// 		}
+// 	}, []);
 
-	return (
-		<div className="my-8" ref={modelRef}>
-			<model-viewer
-				src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
-				alt="Astronaut Model"
-				auto-rotate
-				camera-controls
-				style={{
-					width: "100%",
-					height: "500px",
-					backgroundColor: "lightgray",
-				}}
-			/>
-		</div>
-	);
-};
+// 	return (
+// 		<div className="my-8" ref={modelRef}>
+// 			<model-viewer
+// 				src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+// 				alt="Astronaut Model"
+// 				auto-rotate
+// 				camera-controls
+// 				style={{
+// 					width: "100%",
+// 					height: "500px",
+// 					backgroundColor: "lightgray",
+// 				}}
+// 			/>
+// 		</div>
+// 	);
+// };
 
-export default ARCapsule;
+// export default ARCapsule;
